2b6ba4d1d9473331194550c5452ddb5a14a8e966
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-03-22 07:38:42 +0200

    Learn About Image, Text and Icons

--- lib/Stage 1/Exercise_1.dart
+++ lib/Stage 1/Exercise_1.dart
@@ -3,10 +3,11 @@ import 'package:flutter/material.dart';
 class Cards extends StatelessWidget {
   const Cards({super.key});
 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Card Layout'),
+        title: const Text('Card Layout'),
       ),
       body: Center(
         child: Card(
@@ -14,15 +15,23 @@ class Cards extends StatelessWidget {
           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
           child: SizedBox(
             width: 200,
-            child: Column(children: [
-              Image.asset('assets/images/3079588.jpg'),
-              const SizedBox(height: 20),
-              Text('Machine Learning',
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Image.asset('assets/images/3079588.jpg', width: 120, height: 120),
+                    const SizedBox(height: 20),
+
+                    const Text('Machine learning allow computer to learn from the pattern',
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+              ]),
+
+
+                const SizedBox(height: 20),
+                const Text('Machine Learning',
                   style: TextStyle(color: Colors.blue, fontSize: 20)),
-              const SizedBox(height: 20),
-              Text('Machine learning allow computer to learn from the pattern',
-                  style: TextStyle(fontWeight: FontWeight.bold))
-            ]),
+            ]
+            ),
           ),
         ),
       ),
