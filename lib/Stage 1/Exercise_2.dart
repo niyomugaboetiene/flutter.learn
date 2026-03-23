7b5cde229d19c637d5d8ccba3abd7b923f75f119
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-03-23 07:58:11 +0200

    Learn About Container, Stack, ALignment, Padding, Margin

--- lib/Stage 1/Exercise_2.dart
+++ lib/Stage 1/Exercise_2.dart
@@ -12,6 +12,24 @@ class Exercise extends StatelessWidget {
 
         body: Container(
           width: 250, height: 250, color: Colors.grey,
+
+          child: Stack(
+            children: [
+              Container(
+                width: 250, height: 150, color: Colors.blue,
+              ),
+            
+              const Align(
+                alignment: Alignment.bottomRight,
+                child: Icon(Icons.heat_pump_rounded, color: Colors.red),
+              ),
+
+              const Align(
+                 alignment: Alignment.topLeft,
+                 child: Text('Hello this is Stack Example'),
+              )
+            ],
+          ),
         ),
     );
   }
