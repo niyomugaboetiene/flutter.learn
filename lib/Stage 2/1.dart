8a429c39469eb31b1215fea901c5d4de7e6eb3fb
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-03-23 09:11:48 +0200

    Learn About ListView & GridView

--- lib/Stage 2/1.dart
+++ lib/Stage 2/1.dart
@@ -1,6 +1,5 @@
 // ListView & GridView
 
-
 import 'package:flutter/material.dart';
 
 class Lists extends StatelessWidget {
@@ -9,18 +8,26 @@ class Lists extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Buttons')),
-      body: Center(
-      child:  ListView(
-        children: [
-            const ListTile(title: Text('List 1')),
-            const ListTile(title: Text('List 2')),
-            const ListTile(title: Text('List 3')),
-            const ListTile(title: Text('List 4')),
-            const ListTile(title: Text('List 5')),
-        ],
-      )
-    )
-    );
+        appBar: AppBar(title: const Text('Buttons')),
+        body: Center(
+          //   child:  ListView(
+          //     children: [
+          //         const ListTile(title: Text('List 1')),
+          //         const ListTile(title: Text('List 2')),
+          //         const ListTile(title: Text('List 3')),
+          //         const ListTile(title: Text('List 4')),
+          //         const ListTile(title: Text('List 5')),
+          //     ],
+          //   )
+
+          child: ListView.builder(
+            itemCount: 5,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text("Item $index"),
+              );
+            },
+          ),
+        ));
   }
 }
