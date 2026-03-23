7394711b1154d0b0f7ff86c3683cedf69629e9fe
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-03-23 08:07:37 +0200

    Learn About Container, Stack, ALignment, Padding, Margin

--- lib/Stage 1/3.dart
+++ lib/Stage 1/3.dart
@@ -3,20 +3,18 @@
 import 'package:flutter/material.dart';
 
 class Buttons extends StatelessWidget {
-  const Buttons({ super.key });
+  const Buttons({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Buttons')),
-
       body: ElevatedButton(
         onPressed: () => {},
         child: const Text("Click me"),
         style: ElevatedButton.styleFrom(
-          backgroundColor: Colors.blue, padding: const EdgeInsets.all(15)
-        ),
+            backgroundColor: Colors.blue, padding: const EdgeInsets.all(15)),
       ),
-    )
+    );
   }
 }
