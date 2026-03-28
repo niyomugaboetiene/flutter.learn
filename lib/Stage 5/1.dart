42a6e60c05594b2926955d6007b35e3b5eb4fde5
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-03-28 08:43:28 +0200

    Learn About http package, parse JSON data, Display API In ListView

--- lib/Stage 5/1.dart
+++ lib/Stage 5/1.dart
@@ -1,2 +1,9 @@
 // * http package
 
+import 'package:http/http.dart' as http;
+
+Future<void> fetchData() async {
+  final response = await http.get(Uri.parse("https://jsonplaceholder.typicode.com/posts"));
+
+  print(response.body)
+}
