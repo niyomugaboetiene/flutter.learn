0e355fd5a70ae53f65d9018121ccca25b907e753
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-03-29 07:57:32 +0200

    Building Mobile  Weather App

---
+++ lib/Stage 5/WeatherApp.dart
@@ -0,0 +1,37 @@
+class Weather {
+  final String cityName;
+  final String tamperature;
+  final String feelsLike;
+  final int humidity;
+  final String description;
+  final String main;
+  final String icon;
+  final double windSpeed;
+  final String country;
+
+  Weather({
+    required this.cityName,
+    required this.tamperature,
+    required this.feelsLike,
+    required this.humidity,
+    required this.description,
+    required this.main,
+    required this.icon,
+    required this.windSpeed,
+    required this.country,
+  });
+
+  factory Weather.fromJson(Map<String, dynamic> json) {
+    return Weather(
+        cityName: json['cityName'],
+        tamperature: json['tamperature'],
+        feelsLike: json['feelsLike'],
+        humidity: json['humidity'],
+        description: json['description'],
+        main: json['main'],
+        icon: json['icon'],
+        windSpeed: json['windSpeed'],
+        country: json['country']
+    );
+  }
+}
