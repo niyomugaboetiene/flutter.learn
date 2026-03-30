51793ea4d191abb70986ada06c1b32b49f3382ff
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-03-30 15:31:11 +0200

    [Coding] learning shared_preference in flutter

--- lib/Stage 6/1.dart
+++ lib/Stage 6/1.dart
@@ -4,24 +4,53 @@
 
 import 'package:shared_preferences/shared_preferences.dart';
 
-void preference() async {
-  final prefs =
-      await SharedPreferences.getInstance(); // to initialize preference
+// void preference() async {
+//   final prefs =
+//       await SharedPreferences.getInstance(); // to initialize preference
 
-  // saving the data
-  await prefs.setString('name', 'Etiene'); // save data
-  await prefs.setInt('age', 20);
-  await prefs.setBool('isLoggedIn', true);
+// saving the data
+//   await prefs.setString('name', 'Etiene'); // save data
+//   await prefs.setInt('age', 20);
+//   await prefs.setBool('isLoggedIn', true);
 
 // reading the data
-  final String? name = await prefs.getString('name') ?? "no name";
-  final int? age = await prefs.getInt('age') ?? 0;
-  final bool isLogged = await prefs.getBool('isLoggedIn') ?? false;
+//   final String? name = await prefs.getString('name') ?? "no name";
+//   final int? age = await prefs.getInt('age') ?? 0;
+//   final bool isLogged = await prefs.getBool('isLoggedIn') ?? false;
 
-  // removing data
-  // 1.removing all data
-  await prefs.clear();
+// removing data
+// 1.removing all data
+//   await prefs.clear();
 
-  // 2. removing specific data
-  await prefs.remove('name');
+// 2. removing specific data
+//   await prefs.remove('name');
+// }
+
+import 'package:flutter/material.dart';
+
+class Preference extends StatefulWidget {
+  const Preference({super.key});
+
+  @override
+  State<Preference> createState() => _PreferenceState();
+}
+
+class _PreferenceState extends State<Preference> {
+  final TextEditingController username = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Preference Screen")),
+      body: Column(
+        children: [
+           TextField(
+               controller: username,
+        decoration: InputDecoration(
+            hintText: "Enter your username", labelText: "Username"),
+      ),
+        ],
+        ) 
+    );
+  }
 }
