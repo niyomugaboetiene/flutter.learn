387c3a6460bd46f3ecf83cffc31aa50690d1fa6d
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-03-30 15:15:53 +0200

    [Coding] learning shared_preference in flutter

---
+++ lib/Stage 6/1.dart
@@ -0,0 +1,21 @@
+// SharedPreferences (key-value storage): is a way to store small data locally on the device
+// SharedPreference allow our app to remember a small data even if our app is closed
+// Get instance → Save data → Read data → Remove data
+
+import 'package:shared_preferences/shared_preferences.dart';
+
+void preference() async {
+  final prefs =
+      await SharedPreferences.getInstance(); // to initialize preference
+
+  // saving the data
+  await prefs.setString('name', 'Etiene'); // save data
+  await prefs.setInt('age', 20);
+  await prefs.setBool('isLoggedIn', true);
+
+// reading the data
+
+  final String? name = await prefs.getString('name') ?? "no name";
+  final int? age = await prefs.getInt('age') ?? 0;
+  final bool isLogged = await prefs.getBool('isLoggedIn') ?? false;
+}
