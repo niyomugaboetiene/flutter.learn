64dde7ec90c257f7d4099cf78ab34be45d377cd9
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-03-30 16:35:44 +0200

    [Coding] learning shared_preference in flutter

---
+++ lib/Stage 6/Theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Theme extends StatefulWidget {
+  const Theme({super.key});
+
+  @override
+  State<Theme> createState() => _ThemeState();
+}
+
+class _ThemeState extends State<Theme> {
+  
+}
