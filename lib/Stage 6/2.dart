8b6fc5d2f921c3321ca309df9ff2c9ba47155f0f
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-03-31 15:44:42 +0200

    [Coding] learning Hive/NoSQL storage for flutter

--- lib/Stage 6/2.dart
+++ lib/Stage 6/2.dart
@@ -1,8 +1,11 @@
 // Hive = fast, lightweight NoSQL database for Flutter/Dart
 // flutter pub add hive:^2.2.3
 
+import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+
+part 'task.g.dart'; // used for code generation;
 
 // the other configuration are done in maain.dart
 void hiveOperatiob() async {
@@ -21,4 +24,17 @@ void hiveOperatiob() async {
   box.delete('name');
   box.deleteAt(0); // delete the value of first index
   box.clear(); // removes everything in the box
+
+ 
 }
+ // * storing object: you will need a model class and TypeAdapter
+  @HiveType(typeId: 0)
+  class Task {
+        @HiveField(0)
+        String title;
+
+        @HiveField(1)
+        bool isDone;
+
+        Task({ required this.title, this.isDone = false });
+  }
