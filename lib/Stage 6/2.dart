df05fa9148267bbdbc2aaeb9ffd355539d459190
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-03-31 16:07:09 +0200

    adding buil_runner package in pubspec.yaml

--- lib/Stage 6/2.dart
+++ lib/Stage 6/2.dart
@@ -5,28 +5,28 @@ import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
-part 'task.g.dart'; // used for code generation -> it is like translator which convert your model class into format hive can store;
+part '2.g.dart'; // used for code generation -> it is like translator which convert your model class into format hive can store;
 
 // the other configuration are done in maain.dart
-void hiveOperatiob() async {
-  var box =
-      await Hive.box('MyBox'); // box is storage which will store your data
+// void hiveOperatiob() async {
+//   var box =
+//       await Hive.box('MyBox'); // box is storage which will store your data
   // Basic operation
   // 1. Save data
-  box.put('name', 'etiene');
-  box.put('age', 2);
+//   box.put('name', 'etiene');
+//   box.put('age', 2);
 
   // 2. load the data
-  var name = box.get('name');
-  var age = box.get('age');
+//   var name = box.get('name');
+//   var age = box.get('age');
 
   // delete data
-  box.delete('name');
-  box.deleteAt(0); // delete the value of first index
-  box.clear(); // removes everything in the box
+//   box.delete('name');
+//   box.deleteAt(0); // delete the value of first index
+//   box.clear(); // removes everything in the box
 
  
-}
+// }
  // * storing object: you will need a model class and TypeAdapter
   @HiveType(typeId: 0) // unique typeId for this model class
   class Task {
