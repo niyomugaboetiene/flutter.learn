720bb9e414e04fd88eefbcd81d9ba38896b97b18
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-04-04 08:38:29 +0200

    [Coding] learning Drift/SQLite (local database) storage for flutter

--- lib/Stage 6/3.dart
+++ lib/Stage 6/3.dart
@@ -1,4 +1,14 @@
 import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
 import 'package:drift_flutter/drift_flutter.dart';
-import 'package:';
+import 'package:path_privider/path_provider.dart';
+import 'dart:io';
+import 'package:path/path.dart' as p;
+
+// Create a table structure
+class Task extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get title => text();
+  TextColumn get description => text();
+  BoolColumn get 
+}
