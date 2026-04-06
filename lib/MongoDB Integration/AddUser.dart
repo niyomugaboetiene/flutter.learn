75a514cbd9045fb725293a13e9aede8aa50ed71d
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-04-06 10:02:49 +0200

    [Coding] learning Backend integration in mongodb

--- lib/MongoDB Integration/AddUser.dart
+++ lib/MongoDB Integration/AddUser.dart
@@ -42,23 +42,35 @@ class AddUserScreen extends StatefulWidget {
 }
 
 class _AddUserState extends State<AddUserScreen> {
-  TextEditingController full_name = TextEditingController();
-  TextEditingController gender = TextEditingController();
-  TextEditingController roll = TextEditingController();
-  TextEditingController email = TextEditingController();
-  TextEditingController trade = TextEditingController();
-  TextEditingController phone = TextEditingController();
-  TextEditingController location = TextEditingController();
-  TextEditingController classes = TextEditingController();
-  TextEditingController passwrod = TextEditingController();
-  
+  final TextEditingController full_name = TextEditingController();
+  final TextEditingController gender = TextEditingController();
+  final TextEditingController roll = TextEditingController();
+  final TextEditingController email = TextEditingController();
+  final TextEditingController trade = TextEditingController();
+  final TextEditingController phone = TextEditingController();
+  final TextEditingController location = TextEditingController();
+  final TextEditingController classes = TextEditingController();
+  final TextEditingController password = TextEditingController();
+
+@override
+  void initState() {
+    super.initState();
+  }
+
+  void addUser() async {
+    await Adduser.getUser(
+         full_name, gender, roll, email, trade, phone,
+        location, classes, password
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Add Student"),),
-      body: Column(
-
+      appBar: AppBar(
+        title: Text("Add Student"),
       ),
+      body: Column(),
     );
   }
 }
