f6e1becd8c78a9da14fdde8b61da8f9dbd113388
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-04-08 07:21:38 +0200

    [Coding] learning Backend integration in mongodb

--- lib/MongoDB Integration/SelectStudent.dart
+++ lib/MongoDB Integration/SelectStudent.dart
@@ -1,1 +1,38 @@
 import 'package:flutter/material.dart';
+
+class Student {
+  String full_name;
+  String gender;
+  String roll;
+  String email;
+  String trade;
+  String phone;
+  String location;
+  String classes;
+  String password;
+
+  Student(
+      {required this.full_name,
+      required this.gender,
+      required this.roll,
+      required this.email,
+      required this.trade,
+      required this.phone,
+      required this.location,
+      required this.classes,
+      required this.password});
+
+  factory Student.fromJson(Map<String, dynamic> json) {
+    return Student(
+        full_name: json['full_name'],
+        gender: json['gender'],
+        roll: json['roll'],
+        email: json['email'],
+        trade: json['trade'],
+        phone: json['phone'],
+        location: json['location'],
+        classes: json['classes'],
+        password: json['password']
+    );
+  }
+}
