ca922864a0da8eddd2b8a006ef269673001cc10c
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-04-11 07:56:10 +0200

    [flutter] implementation of mongodb api

--- lib/MongoDB Integration/UpdateStudent.dart
+++ lib/MongoDB Integration/UpdateStudent.dart
@@ -14,36 +14,24 @@ class UpdateScreen extends StatefulWidget {
 class _UpdateScreenState extends State<UpdateScreen> {
   static const BaseUrl = "http://localhost:5000";
 
-  static Future<Map<String, dynamic>> getUser(
-      String full_name,
-      String gender,
-      String roll,
-      String email,
-      String trade,
-      String phone,
-      String location,
-      String classes,
-      String password) async {
+  final TextEditingController full_name = TextEditingController();
+  final TextEditingController gender = TextEditingController();
+  final TextEditingController roll = TextEditingController();
+  final TextEditingController email = TextEditingController();
+  final TextEditingController trade = TextEditingController();
+  final TextEditingController phone = TextEditingController();
+  final TextEditingController location = TextEditingController();
+  final TextEditingController classes = TextEditingController();
+  final TextEditingController password = TextEditingController();
 
-    final response = await http.post(Uri.parse("$BaseUrl/student/register"),
-        headers: {"Content-Type": "application/json"},
+  Future<void> updateUser() async {
+    final response =
+        await http.put(Uri.parse("$BaseUrl/student/update/${widget.id}"),
+        headers: { "Content-Type" : "application/json" },
         body: jsonEncode({
-          "full_name": full_name,
-          "gender": gender,
-          "roll": roll,
-          "email": email,
-          "trade": trade,
-          "phone": phone,
-          "location": location,
-          "classe": classes,
-          "password": password
-        }));
-
-    if (response.statusCode == 200 || response.statusCode == 201) {
-      return jsonDecode(response.body);
-    } else {
-      throw Exception("Failed to add user");
-    }
+          
+        })
+        );
   }
 
   @override
