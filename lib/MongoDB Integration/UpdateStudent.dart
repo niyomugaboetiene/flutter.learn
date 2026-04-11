3a12164dc14c4af799a500b9b4e707bf737856ed
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-04-11 07:49:41 +0200

    [flutter] implementation of mongodb api

--- lib/MongoDB Integration/UpdateStudent.dart
+++ lib/MongoDB Integration/UpdateStudent.dart
@@ -12,8 +12,40 @@ class UpdateScreen extends StatefulWidget {
 }
 
 class _UpdateScreenState extends State<UpdateScreen> {
+  static const BaseUrl = "http://localhost:5000";
 
-  
+  static Future<Map<String, dynamic>> getUser(
+      String full_name,
+      String gender,
+      String roll,
+      String email,
+      String trade,
+      String phone,
+      String location,
+      String classes,
+      String password) async {
+
+    final response = await http.post(Uri.parse("$BaseUrl/student/register"),
+        headers: {"Content-Type": "application/json"},
+        body: jsonEncode({
+          "full_name": full_name,
+          "gender": gender,
+          "roll": roll,
+          "email": email,
+          "trade": trade,
+          "phone": phone,
+          "location": location,
+          "classe": classes,
+          "password": password
+        }));
+
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception("Failed to add user");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold();
