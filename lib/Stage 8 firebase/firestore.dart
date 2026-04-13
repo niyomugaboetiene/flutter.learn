344beab3e20a20b1b0852aa339bea9d80b28fa90
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-04-13 08:17:04 +0200

    [coding] learn abou cloud firestore

--- lib/Stage 8 firebase/firestore.dart
+++ lib/Stage 8 firebase/firestore.dart
@@ -28,3 +28,9 @@ Future<void> updateUser() async {
       .doc("user_123")
       .update({'age': 19});
 }
+
+// delete user
+
+Future<void> deleteUser() async {
+  await FirebaseFirestore.instance.collection("users").doc("user_123").delete();
+}
