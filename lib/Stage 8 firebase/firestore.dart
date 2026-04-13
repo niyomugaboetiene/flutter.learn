fcb675e46fe89958711a24ca60cfa990fcb80efb
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-04-13 08:16:02 +0200

    [coding] learn abou cloud firestore

--- lib/Stage 8 firebase/firestore.dart
+++ lib/Stage 8 firebase/firestore.dart
@@ -6,12 +6,12 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 // add user
 Future<void> addUser() async {
   await FirebaseFirestore.instance
-      .collection("users").doc("user_123")
+      .collection("users")
+      .doc("user_123")
       .set({'name': 'etiene', 'age': 12, 'email': "et@gmail.com"});
 }
 
 // select users/ read
-
 Future<void> getUsers() async {
   QuerySnapshot snapshot =
       await FirebaseFirestore.instance.collection("users").get();
@@ -21,4 +21,10 @@ Future<void> getUsers() async {
   }
 }
 
-
+// update user
+Future<void> updateUser() async {
+  await FirebaseFirestore.instance
+      .collection("users")
+      .doc("user_123")
+      .update({'age': 19});
+}
