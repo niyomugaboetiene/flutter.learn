096ccca6fb0fa39e3e5662f7992c2ea23ba2c144
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-04-15 09:18:24 +0200

    [coding] fixing about imbalance class in my model

---
+++ lib/Stage 8 firebase/firebase_storage.dart
@@ -0,0 +1,10 @@
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'dart:io';
+
+// 1. upload image
+
+Future<String?> uploadImage() async {
+  final picker = ImagePicker();
+  final pickedFIle = await picker.pickImage(source: ImageSource.gallery);
+}
